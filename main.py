9cc583ebc76cbb3525ed9cf7f3d821e38bec9fef
kemat0723 <kemat@misena.edu.co>
2024-11-06 19:47:54 -0500

    Abordamos el slide 13 del material sciki-learn

---
+++ main.py
@@ -0,0 +1,16 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+# Cargar el conjunto de datos Iris
+iris = load_iris()
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
+# Crear el clasificador de vecinos más cercanos
+clf = KNeighborsClassifier(n_neighbors=3)
+# Entrenar el clasificador
+clf.fit(X_train, y_train)
+# Predecir las etiquetas para los dtos de prueba
+y_pred = clf.predict(X_test)
+# Calcular la precisión del clasificador
+accuracy = accuracy_score(y_test, y_pred)
+print('Precisión del clasificador:', accuracy)
