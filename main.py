a4d130ce965f5a799aa01129dbb58c518b7fc284
kemat0723 <kemat@misena.edu.co>
2024-11-06 21:16:03 -0500

    Hemos abordado el slide 15 del material de apoyo

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from sklearn.datasets import load_iris
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
-# Cargar el conjunto de datos Iris
-iris = load_iris()
-X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=42)
-# Crear el clasificador de vecinos más cercanos
-clf = KNeighborsClassifier(n_neighbors=3)
-# Entrenar el clasificador
-clf.fit(X_train, y_train)
-# Predecir las etiquetas para los dtos de prueba
-y_pred = clf.predict(X_test)
-# Calcular la precisión del clasificador
-accuracy = accuracy_score(y_test, y_pred)
-print('Precisión del clasificador:', accuracy)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Cargar el conjunto de datos California Housing
+California = fetch_california_housing()
+X_train, X_test, y_train, y_test = train_test_split(California.data, California.target, test_size=0.2, random_state=42)
+
+# Crear el modelo de regresion lineal
+model = LinearRegression()
+
+# Entrenar el modelo
+model.fit(X_train, y_train)
+
+# Predecir los precios de las viviendas para los datos de prueba
+y_pred = model.predict(X_test)
+
+# Calcular el error cuadratico medio (MSE)
+mse = mean_squared_error(y_test, y_pred)
+print("Error cuadratico medio (MSE):", mse)
